f3174c7ef717c70849e385d47f309fb4014e48c0
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-07-29 01:59:31 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,20 +4,20 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://ok:lol@cluster1.udhzs7r.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "10e3ed833b0d09699973420d45359409"]
+        API_ID = [int, 4665778]
+        BOT_TOKEN = [str, "5322443077:AAH935gkdwLxgiBVbxBbcQeYlfSFZKncqM8"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
         SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
         AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        OWNER_ID = [int, 5531584953]
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
